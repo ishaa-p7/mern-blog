f729c90e8b16f7610b8ef6467f47c2d518c6df87
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-22 15:08:29 +0530

    signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,24 +1,41 @@
-import { Button, TextInput } from 'flowbite-react'
+import { Alert, Button, Spinner, TextInput } from 'flowbite-react'
 import { Label } from 'flowbite-react'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 function SignUp() {
   const [formData,setFormData]=useState({});
+  const[errorMessage,setErrorMessage]=useState(null);
+  const [loading,setLoading]=useState(false);
+  const navigate=useNavigate();
 const handleChange=(e)=>{
-  setFormData({...formData,[e.target.id]:e.target.value});
+  setFormData({...formData,[e.target.id]:e.target.value.trim()});
 };
 const handleSubmit=async(e)=>{
   e.preventDefault();
+  if(!formData.username||!formData.email||!formData.password){
+    return setErrorMessage('Please fill out all fields');
+  }
   try{
+    setLoading(true);
+    setErrorMessage(null);
 const res=await fetch('/api/auth/signup',{
   method:'POST',
-  header:{'Content-Type':'application/json'},
+  headers:{'Content-Type':'application/json'},
   body:JSON.stringify(formData),
 });
   const data=await res.json();
+  if(data.success===false){
+    return setErrorMessage(data.message);
+  }
+  setLoading(false);
+  if(res.ok){
+    navigate('/sign-in');
+  }
 }
 catch(error){
+  setErrorMessage(error.message);
+  setLoading(false);
 }
 }; 
 
@@ -60,8 +77,15 @@ Blog
             placeholder='Password'
             id='password'onChange ={handleChange}/>
           </div>
-          <Button gradientDuoTone='purpleToPink' type='submit'>
-            Sign Up
+          <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+            {
+              loading?(
+                <>
+                <Spinner size='sm'/>
+                <span className='pl-3'>Loading...</span>
+                </>
+              ):'Sign Up'
+            }
             </Button>
         </form>
         <div className="flex gap-2 text-sm mt-5">
@@ -70,6 +94,13 @@ Blog
           Sign In
           </Link>
         </div>
+        {
+          errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )
+        }
       </div>
     </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'http://localhost:3000/api',
         changeOrigin: true,
         secure:false,
-       // rewrite: (path) => path.replace(/^\/api/, ''),
+       rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
