396465691fa402c3bed09745c874d5ebe6c7897c
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-22 12:52:19 +0530

    new

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+import authRoues from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -13,7 +15,21 @@ connect(process.env.MONGO).then(
     });
 
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server running.');
+});
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoues);
+
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
 });
