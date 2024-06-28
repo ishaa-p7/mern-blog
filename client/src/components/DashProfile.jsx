3fedd367891553a89a4efa16a6e62bd0b388e2df
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-29 00:36:03 +0530

    create post page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -24,7 +24,7 @@ import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
 
 function DashProfile() {
-    const {currentUser,error}=useSelector(state=>state.user);
+    const {currentUser,error,loading}=useSelector(state=>state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
    // const [imageFileUploading, setImageFileUploading] = useState(false);
@@ -188,9 +188,21 @@ function DashProfile() {
            defaultValue={currentUser.email}onChange={handleChange}/>
            <TextInput type='password' id='password' placeholder='password'onChange={handleChange}
         />
-        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-            Update
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline disabled={loading}>
+            {loading?'Loading...':'Update'}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
+
         </form>
         <div className="text-red-500 flex justify-between mt-5">
             <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
