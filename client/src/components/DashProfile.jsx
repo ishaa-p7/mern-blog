bb234eb1ae27b4fec550fe7e5ab99e44a15f3dda
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-28 22:38:17 +0530

    profilepage complete

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -27,11 +27,14 @@ function DashProfile() {
     const {currentUser}=useSelector(state=>state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
-    const [imageFileUploading, setImageFileUploading] = useState(false);
+   // const [imageFileUploading, setImageFileUploading] = useState(false);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
     const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+    const [updateUserError, setUpdateUserError] = useState(null);
       const [formData, setFormData] = useState({});
     const filePickerRef = useRef();
+    const dispatch=useDispatch();
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if (file) {
@@ -75,15 +78,52 @@ function DashProfile() {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
               setImageFileUrl(downloadURL);
               setFormData({ ...formData, profilePicture: downloadURL });
-              setImageFileUploading(false);
+              //setImageFileUploading(false);
             });
           }
         );
       };
+
+      const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+      };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        setUpdateUserError(null);
+        setUpdateUserSuccess(null);
+        if (Object.keys(formData).length === 0) {
+          setUpdateUserError('No changes made');
+          return;
+        }
+       
+        try {
+          dispatch(updateStart());
+          const res = await fetch(`/api/user/update/${currentUser._id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            dispatch(updateFailure(data.message));
+           setUpdateUserError(data.message);
+          } else {
+            dispatch(updateSuccess(data));
+            setUpdateUserSuccess("User's profile updated successfully");
+          }
+        } catch (error) {
+          dispatch(updateFailure(error.message));
+          setUpdateUserError(error.message);
+        }
+      };
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-centr font-semibold text-3xl'>Profile</h1>
-        <form className='flex flex-col gap-4'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type="file"
           accept='image/*'
@@ -103,14 +143,14 @@ function DashProfile() {
             }`}
           />
            </div>
-           {imageFileUploadError && (
+            {imageFileUploadError && (
           <Alert color='failure'>{imageFileUploadError}</Alert>
         )}
            <TextInput type='text' id='username' placeholder='username'
-           defaultValue={currentUser.username}/>
+           defaultValue={currentUser.username}onChange={handleChange} />
            <TextInput type='email' id='email' placeholder='email'
-           defaultValue={currentUser.email}/>
-           <TextInput type='password' id='password' placeholder='password'
+           defaultValue={currentUser.email}onChange={handleChange}/>
+           <TextInput type='password' id='password' placeholder='password'onChange={handleChange}
         />
         <Button type='submit' gradientDuoTone='purpleToBlue' outline>
             Update
@@ -120,6 +160,16 @@ function DashProfile() {
             <span className='cursor-pointer'>Delete Account</span>
             <span className='cursor-pointer'>Sign Out</span>
         </div>
+        {updateUserSuccess && (
+        <Alert color='success' className='mt-5'>
+          {updateUserSuccess}
+        </Alert>
+      )}
+       {updateUserError && (
+        <Alert color='failure' className='mt-5'>
+          {updateUserError}
+        </Alert>
+      )}
     </div>
   )
 }
