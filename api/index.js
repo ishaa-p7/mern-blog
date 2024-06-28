00026f9a174a658a400e7bf26e7b7caf88b6151c
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-29 04:43:41 +0530

    create a post api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
-import authRoues from './routes/auth.route.js';
+import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import postRoutes from './routes/post.route.js'
 
 dotenv.config();
 
@@ -19,12 +20,14 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.listen(3000,()=>{
     console.log('Server running.');
 });
 
 app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoues);
+app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes);
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
