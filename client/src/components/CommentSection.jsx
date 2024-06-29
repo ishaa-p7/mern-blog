0b1b2e0d5cbc73c75d6a1e454bb9bee109d4dde1
ishaa-p7 <ishapatel1207@gmail.com>
2024-06-30 00:00:42 +0530

    add like functionality to comments

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -56,6 +56,34 @@ export default function CommentSection({ postId }) {
     getComments();
   }, [postId]);
 
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -122,7 +150,7 @@ export default function CommentSection({ postId }) {
             <Comment
               key={comment._id}
               comment={comment}
-              
+              onLike={handleLike}
             />
           ))}
         </>
